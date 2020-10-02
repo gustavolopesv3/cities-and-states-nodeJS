778fb82d13f8676d37af99f233f6d41a645d7bc2
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-10-01 19:52:05 -0500

    projeto concluido

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const fs = require('fs').promises;
 
+init();
+
+async function init() {
+  await createFiles();
+
+  await getStatesWithMoreOrLessCities(true);
+  await getStatesWithMoreOrLessCities(false);
+
+  await getBiggerOrSmallerNameCities(true);
+  await getBiggerOrSmallerNameCities(false);
+
+  await getBiggerOrSmallerCityName(true);
+  await getBiggerOrSmallerCityName(false);
+}
+
 async function createFiles() {
   let data = await fs.readFile('./files/Estados.json');
   const states = JSON.parse(data);
@@ -11,25 +26,125 @@ async function createFiles() {
     const stateCities = cities.filter((city) => city.Estado === state.ID);
     await fs.writeFile(
       `./states/${state.Sigla}.json`,
-      JSON.stringify(stateCities, null, 2)
+      JSON.stringify(stateCities)
     );
   }
 }
 
-createFiles();
+async function getCitiesCount(uf) {
+  const data = await fs.readFile(`./states/${uf}.json`);
+  const cities = JSON.parse(data);
+  return cities.length;
+}
 
-// async function readFile() {
-//   let data = await fs.readFile('./files/Cidades.json');
-//   const states = JSON.parse(data);
-//   data = await fs.readFile('./files/Estados.json');
-//   const cities = JSON.parse(data);
-//   for (state of states) {
-//     const stateCities = cities.filter((city) => city.Estado === state.ID);
-//     console.log(state);
-//     await fs.writeFile(
-//       `./states/${state.Sigla}.json`,
-//       JSON.stringify(stateCities)
-//     );
-//   }
-// }
-// readFile();
+async function getStatesWithMoreOrLessCities(more) {
+  const states = JSON.parse(await fs.readFile('./files/Estados.json'));
+  const list = [];
+
+  for (state of states) {
+    const count = await getCitiesCount(state.Sigla);
+    list.push({ uf: state.Sigla, count });
+  }
+
+  list.sort((a, b) => {
+    if (a.count < b.count) return 1;
+    else if (a.count > b.count) return -1;
+    else return 0;
+  });
+
+  const result = [];
+  if (more) {
+    list
+      .slice(0, 5)
+      .forEach((item) => result.push(item.uf + ' - ' + item.count));
+  } else {
+    list.slice(-5).forEach((item) => result.push(item.uf + ' - ' + item.count));
+  }
+
+  console.log(result);
+}
+
+async function getBiggerOrSmallerNameCities(bigger) {
+  const states = JSON.parse(await fs.readFile('./files/Estados.json'));
+  const result = [];
+
+  for (state of states) {
+    let city;
+    if (bigger) {
+      city = await getBiggerName(state.Sigla);
+    } else {
+      city = await getSmallerName(state.Sigla);
+    }
+
+    result.push(city.Nome + ' - ' + state.Sigla);
+  }
+  console.log(result);
+}
+
+async function getBiggerName(uf) {
+  const cities = JSON.parse(await fs.readFile(`./states/${uf}.json`));
+
+  let result;
+
+  cities.forEach((city) => {
+    if (!result) result = city;
+    else if (city.Nome.length > result.Nome.length) result = city;
+    else if (
+      city.Nome.length === result.Nome.length &&
+      city.Nome.toLowerCase() < result.Nome.toLowerCase()
+    )
+      result = city;
+  });
+
+  return result;
+}
+
+async function getSmallerName(uf) {
+  const cities = JSON.parse(await fs.readFile(`./states/${uf}.json`));
+
+  let result;
+
+  cities.forEach((city) => {
+    if (!result) result = city;
+    else if (city.Nome.length < result.Nome.length) result = city;
+    else if (
+      city.Nome.length === result.Nome.length &&
+      city.Nome.toLowerCase() < result.Nome.toLowerCase()
+    )
+      result = city;
+  });
+
+  return result;
+}
+
+async function getBiggerOrSmallerCityName(bigger) {
+  const states = JSON.parse(await fs.readFile('./files/Estados.json'));
+  const list = [];
+  for (state of states) {
+    let city;
+    if (bigger) {
+      city = await getBiggerName(state.Sigla);
+    } else {
+      city = await getSmallerName(state.Sigla);
+    }
+    list.push({ name: city.Nome, uf: state.Sigla });
+  }
+  const result = list.reduce((acc, current) => {
+    if (bigger) {
+      if (acc.name.length > current.name.length) return acc;
+      else if (acc.name.length < current.name.length) return current;
+      else
+        return acc.name.toLowerCase() < current.name.toLowerCase()
+          ? acc
+          : current;
+    } else {
+      if (acc.name.length < current.name.length) return acc;
+      else if (acc.name.length > current.name.length) return current;
+      else
+        return acc.name.toLowerCase() < current.name.toLowerCase()
+          ? acc
+          : current;
+    }
+  });
+  console.log(result.name + ' - ' + result.uf);
+}
